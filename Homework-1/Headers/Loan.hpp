739f4b31cc89ba531fbfb3cb653b77ed104d179a
Pharos1 <pharosisdoingnothing@gmail.com>
2025-12-01 23:53:24 +0200

    Fixed Loan getters not being const

--- Homework-1/Headers/Loan.hpp
+++ Homework-1/Headers/Loan.hpp
@@ -12,10 +12,10 @@ public:
 	Loan() = default;
 	Loan(std::string isbn, std::string memberId, std::string startDate, std::string dueDate, bool returned);
 
-	const std::string& getIsbn() { return isbn; }
-	const std::string& getMemberId() { return memberId; }
-	const std::string& getStartDate() { return startDate; }
-	const std::string& getDueDate() { return dueDate; }
+	const std::string& getIsbn() const { return isbn; }
+	const std::string& getMemberId() const { return memberId; }
+	const std::string& getStartDate() const { return startDate; }
+	const std::string& getDueDate() const { return dueDate; }
 
 	void setIsbn(std::string isbn) { this->isbn = std::move(isbn); }
 	void setMemberId(std::string memberId) { this->memberId = std::move(memberId); }
